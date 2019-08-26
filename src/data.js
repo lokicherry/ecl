ec9bee7f92a2370ddeb3ce02bda72fb0bdcdf615
lokicherry <rongali.lokesh@gmail.com>
2019-08-26 22:56:12 +0530

    Add data

---
+++ src/data.js
@@ -0,0 +1,285 @@
+const players_profiles = [
+  {
+    name: "Anil",
+    photo: "",
+    base_price: "",
+    role: "BATSMAN",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Anirudh",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Ali",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 2,
+    bowling_type: "FAST",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Balu",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "LEFT"
+  },
+  {
+    name: "Donna",
+    photo: "",
+    base_price: "",
+    role: "BATSMAN",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Faiz",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Harish",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Hemanth",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 3,
+    bowling_type: "FAST",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Lokesh",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 3,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Mahitha",
+    photo: "",
+    base_price: "",
+    role: "BATSMAN",
+    batting_rating: 2,
+    bowling_rating: 1,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Manoj",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+
+  {
+    name: "Mihika",
+    photo: "",
+    base_price: "",
+    role: "Batsman",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Mukesh",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 3,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Naveen Kumar B",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 3,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Nidhuraj",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+
+  {
+    name: "Nikhil",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Prasanna",
+    photo: "",
+    base_price: "",
+    role: "BATSMAN",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Ranganathan",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Ravindra Babu",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 3,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Sandeep",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Shalom",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Saumya",
+    photo: "",
+    base_price: "",
+    role: "BATSMAN",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Sharath",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 3,
+    bowling_type: "FAST",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Sowmya",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Srikanth",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "FAST",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Subbu",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 3,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Syamala",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 1,
+    bowling_rating: 1,
+    bowling_type: "SLOW",
+    batting_type: "RIGHT"
+  },
+  {
+    name: "Vijay",
+    photo: "",
+    base_price: "",
+    role: "ALL_ROUNDER",
+    batting_rating: 2,
+    bowling_rating: 2,
+    bowling_type: "MEDIUM",
+    batting_type: "RIGHT"
+  }
+];
+export default players_profiles;
